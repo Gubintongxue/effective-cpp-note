8b3c4397490ddc35f03a003f79448ea89757b853
gubin <1205408034@qq.com>
2023-05-08 10:19:53 +0800

    更新

---
+++ others's note/effective_notes-master/exceptional_cpp_01/exceptional_cpp_01.cpp
@@ -0,0 +1,29 @@
+﻿// exceptional_cpp_01.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+
+/**
+ * 参考《Exceptional C++》
+ */
+
+void* foo = nullptr;
+
+/**
+ * 1 泛型程序设计与C++标准库
+ */
+
+using namespace std;
+
+//
+//
+
+
+//
+//
+
+
+int main()
+{
+	std::cout << "Hello World!\n";
+}
